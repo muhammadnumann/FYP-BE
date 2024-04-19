a7db9fe18dde7369654e54250d48bf4f5e725061
Muhammad Numan <numanrasheed315@gmail.com>
2024-04-19 18:03:11 +0500

    Working on API

---
+++ src/routes.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import authRouter from './controllers/auth/auth.routes';
+import adminRoutes from './controllers/accounts/accounts.routes';
+import ContactUs from './controllers/contactus/contactus.routes'
+import sectionRouter from './controllers/sections/section.routes'
+
+const router = Router();
+
+router.use('/auth', authRouter);
+router.use('/admin', adminRoutes);
+router.use('/contactus', ContactUs);
+router.use('/section', sectionRouter);
+
+
+export default router;                     
