0f0b398a28fc7148d00650794fb4dd9a7748de20
Muhammad Numan <numanrasheed315@gmail.com>
2024-05-01 15:10:03 +0500

    status

--- src/middleware/fileUpload.ts
+++ src/middleware/fileUpload.ts
@@ -1,9 +1,12 @@
+import multer from 'multer';
 
-import multer from "multer";
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + file.originalname)
+    }
+})
 
-const upload = multer({ dest: 'uploads/' })
-
-
-export default upload
-
-
+export const upload = multer({ storage: storage })
