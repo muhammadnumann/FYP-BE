1df90226e6eca6230f5860be6db5a2d53868ede8
Muhammad Numan <numanrasheed315@gmail.com>
2024-05-18 15:19:31 +0500

    changes

--- src/controllers/accounts/accounts.controller.ts
+++ src/controllers/accounts/accounts.controller.ts
@@ -88,8 +88,9 @@ export const AddAccount = async (req: Request, res: Response) => {
 export const getAllAccounts = async (req: Request, res: Response) => {
   console.log('Get all Account')
 
-  const { pageNo, pageSize = 5 } = req.body;
-  const skip = (pageNo - 1) * pageSize;
+  const { pageNo, pageSize = 5 } = req.query;
+  console.log(req.query)
+  const skip = (Number(pageNo) - 1) * Number(pageSize);
   if (req.headers.authorization) {
     try {
       const token = req.headers.authorization.split(' ')[1];
@@ -101,7 +102,7 @@ export const getAllAccounts = async (req: Request, res: Response) => {
         credentialId: { $ne: new mongoose.Types.ObjectId(currentCredentialId) },
         isDeleted: { $ne: true }
       }).skip(skip)
-        .limit(pageSize);
+        .limit(Number(pageSize));
       ;
       logger.log({
         level: 'debug',
@@ -110,7 +111,14 @@ export const getAllAccounts = async (req: Request, res: Response) => {
       });
       return res.status(200).json({
         success: true,
-        accounts
+        pageNo,
+        pageSize,
+        totalAccounts: (await Accounts.find({
+          credentialId: { $ne: new mongoose.Types.ObjectId(currentCredentialId) },
+          isDeleted: { $ne: true }
+        })).length,
+
+        accounts,
       });
     } catch (e) {
       logger.error({
